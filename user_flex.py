ecba6cf356a5e8f209efafbdcf5bf7b12f42ebf1
Mindi Garcia <mgarcia410.mg@gmail.com>
2023-07-19 16:44:18 -0400

    updated assignment with chaining method

--- user_flex.py
+++ user_flex.py
@@ -9,6 +9,7 @@ class User:
 
     def display_info(self):
         print(f"{self.first_name}\n{self.last_name}\n{self.email}\n{self.age}\n{self.is_rewards_member}\n{self.gold_card_points}\n")
+        return self
 
     def enroll(self):
         if not self.is_rewards_member:
@@ -16,27 +17,22 @@ class User:
             self.gold_card_points = 200
         else:
             print("Sorry you are already a rewards member.")
+        return self
     
     def spend_points(self,amount):
         if amount < self.gold_card_points: 
             self.gold_card_points -= amount
         else:
             print("Sorry you do not have enough Gold card points to redeem this prize.")    
+        return self
 
 user_1 = User("Moira", "Rose", "mrose@sunsetbay.com", "62")
-user_1.display_info()
-user_1.enroll()
-user_1.display_info()
-user_1.spend_points(50)
-user_1.display_info()
-user_1.enroll() #will print out sorry you are already a rewards member after trying to re-enroll
+user_1.display_info().enroll().spend_points(50).display_info().enroll().display_info()
+
 
 user_2 = User("David", "Rose", "drose@roseapothecary.com", "36")
-user_2.display_info()
-user_2.enroll()
-user_2.spend_points(80)
-user_2.display_info()
+user_2.display_info().enroll().spend_points(80).display_info()
+
 
 user_3 = User("Alexis", "Rose", "alittlebitalexis.com", "32")
-user_3.display_info()
-user_3.spend_points(40) #will print out sorry message they do not have enough gold card points to redeem this prize.
+user_3.display_info().spend_points(40)
